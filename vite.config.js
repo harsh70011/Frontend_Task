7f6ecaec6b779eaffc24fb844e45ba762eea13b4
Harsh <harshkaku2001@gmail.com>
2024-06-21 18:04:53 +0530

    Add .gitattributes to handle line endings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+
+
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/your-repo-name/', // Replace with your repository name
+  build: {
+    outDir: 'dist' // This is the default, so you may not need to add this
+  }
+});
